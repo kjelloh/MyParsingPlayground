155262b2b112a265c2e44aa049cfbbc542177a67
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-05-16 13:41:03 +0200

    Pares one digit OK

--- main.cpp
+++ main.cpp
@@ -23,23 +23,58 @@ namespace v0 {
   using Tree = Root;
 
   // Parser (text -> AST)
-  struct Thing {};
-  struct ParseResult {
-    bool success{};
-    Thing thing{};
-    std::string msg;
-    std::string const& s;
-    int next;
-    ParseResult(std::string const& s,Thing const& thing,int next) : s{s},success{true},thing{thing} {}
-    ParseResult(std::string const& s,int next,std::string msg) : s{s},success{false},msg{msg} {}    
+  enum class ThingId {
+    Undefined
+    ,Digit
+    ,Unknown
   };
-  std::ostream& operator<<(std::ostream& os,ParseResult const& pr) {
-    if (pr.s.size()>0) os << "\nSorry, parsing of " << std::quoted(pr.s) << " not yet implemented";
+  struct Thing {
+    ThingId id;
+    std::string value;
+    Thing(ThingId id,std::string value) : id{id},value{value} {}
+  };
+  std::ostream& operator<<(std::ostream& os,Thing const& thing) {
+    switch (thing.id) {
+      case ThingId::Undefined: os << "Undefined"; break;
+      case ThingId::Digit: os << "Digit"; break;
+      case ThingId::Unknown: os << "Unknown"; break;
+    }
     return os;
   }
-  ParseResult parse(std::string const& s) {
-    ParseResult result{s,0,""};
-    return result;
+  struct ParseResult {
+    std::string const& s;
+    int next;
+    bool success;
+    std::string msg;
+    Thing thing;
+    ParseResult(std::string const& s,int next,bool success,std::string msg,Thing thing) 
+      : s{s},next{next},success{success},msg{msg},thing{thing} {}
+  };
+  std::ostream& operator<<(std::ostream& os,ParseResult const& pr) {
+    if (pr.success) {
+      os << pr.thing;
+    }
+    else {
+      os << "Error," << pr.msg;
+    }
+    return os;
+  }
+  ParseResult parse_digit(std::string const& s,int current) {
+    auto next = current;
+    if (next < s.size()) {
+      auto ch = s[next];
+      if (std::isdigit(ch)) {
+        ++next;
+        return ParseResult(s,next,true,"Ok",Thing{ThingId::Digit,std::to_string(ch)});
+      }
+      else return ParseResult(s,current,false,"Expected digit",Thing{ThingId::Unknown,""});
+    }
+    else {
+      return ParseResult(s,current,false,"Expected digit but found end of text",Thing{ThingId::Unknown,""});
+    }
+  }
+  ParseResult parse(std::string const& s,int current) {
+    return parse_digit(s,current);
   }
 
 }
@@ -50,7 +85,7 @@ int main(int argc, char *argv[]) {
   do {
     std::cout << "\nEnter text to parse (input nothing,'q' or 'quit' to quit)>";
     std::getline(std::cin,s);
-    auto result = parser::parse(s);
+    auto result = parser::parse(s,0);
     std::cout << "\n" << result;
   } while (s.size()>0 and s!="q" and s!="quit");
   return 0;
